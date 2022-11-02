7e86e53837465e5b98a83774a6d97cd51403efd3
direva <direva99@gmail.com>
2022-11-02 21:54:38 +0300

    исправлены комментарии по ревью

--- src/test/java/CatTest.java
+++ src/test/java/CatTest.java
@@ -1,23 +1,39 @@
 import com.example.Cat;
 import com.example.Feline;
+import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.mockito.junit.MockitoJUnitRunner;
 
 import java.util.List;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 
+@RunWith(MockitoJUnitRunner.class)
 public class CatTest {
-    Feline feline = new Feline();
-    Cat cat = new Cat(feline);
+    @Mock
+    Feline feline;
+
+    @Before
+    public void init() {
+        MockitoAnnotations.initMocks(this);
+    }
 
     @Test
     public void validateCatSound() {
+        Cat cat = new Cat(feline);
         assertThat(cat.getSound(), is("Мяу"));
     }
 
     @Test
     public void validateCatFood() throws Exception {
-        assertThat(cat.getFood(), is(List.of("Животные", "Птицы", "Рыба")));
+        List<String> expectedFood = List.of("Животные", "Птицы", "Рыба");
+        Mockito.when(feline.eatMeat()).thenReturn(expectedFood);
+        Cat cat = new Cat(feline);
+        assertThat(cat.getFood(), is(expectedFood));
     }
 }
--- src/test/java/LionTest.java
+++ src/test/java/LionTest.java
@@ -1,48 +1,40 @@
 import com.example.Feline;
 import com.example.Lion;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.junit.runners.Parameterized;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.mockito.junit.MockitoJUnitRunner;
 
 import java.util.List;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 
-@RunWith(Parameterized.class)
+@RunWith(MockitoJUnitRunner.class)
 public class LionTest {
-    Feline feline = new Feline();
-    private String sex;
-    private boolean result;
+    @Mock
+    Feline feline;
 
-    public LionTest(String sex, boolean result) {
-        this.sex = sex;
-        this.result = result;
-    }
-
-    @Parameterized.Parameters
-    public static Object[][] getTestData() {
-        return new Object[][] {
-                { "Самец", true },
-                { "Самка", false },
-        };
-    }
-
-    @Test
-    public void validateLionSex() throws Exception {
-        Lion lion = new Lion(feline, sex);
-        assertThat(lion.doesHaveMane(), is(result));
+    @Before
+    public void init() {
+        MockitoAnnotations.initMocks(this);
     }
 
     @Test
     public void validateKittens() throws Exception {
+        Mockito.when(feline.getKittens()).thenReturn(1);
         Lion lion = new Lion(feline, "Самка");
         assertThat(lion.getKittens(), is(1));
     }
 
     @Test
     public void validateLionFood() throws Exception {
+        List<String> expectedFood = List.of("Животные", "Птицы", "Рыба");
+        Mockito.when(feline.eatMeat()).thenReturn(expectedFood);
         Lion lion = new Lion(feline, "Самка");
-        assertThat(lion.getFood(), is(List.of("Животные", "Птицы", "Рыба")));
+        assertThat(lion.getFood(), is(expectedFood));
     }
 }
